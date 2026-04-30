e737cc33a61c14f141b9e3817f26b4fd6801123e
AlanZoloiti <aztrade@ukr.net>
2026-04-30 14:00:18 -0400

    Day 1: Add practice code

--- day01_python/practice.py
+++ day01_python/practice.py
@@ -1,1 +1,9 @@
+data = [100, -20, 0, 50, -10, 200]
 
+positive = [x for x in data if x > 0]
+
+tax = list(map(lambda x: x * 1.2, positive))
+
+result = list(filter(lambda x: x > 100, tax))
+
+print(result)
